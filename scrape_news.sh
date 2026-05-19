0f62bfb227ec26224afa050b0af968f2ef0b5899
Roman Knyazev <antorichn@gmail.com>
2026-05-19 21:24:48 +0300

    scrape news 1.0

---
+++ scrape_news.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+link="https://www.ynetnews.com/category/3082"
+
+# get ynet home page using given link
+home_page=$(wget --no-check-certificate -O - $link 2>/dev/null)
+
+# save links to other articles from home page
+articles=$(grep -oP "https://(www.)?ynetnews.com/article/[a-zA-Z0-9]*" <<< "$home_page" | sort | uniq)
+
+# print articles count
+echo "$(echo "$articles" | wc -l)"
+
+for article in $articles; do   # loop for each article
+
+	# save article text
+	data=$(wget --no-check-certificate -O - $article 2>/dev/null)
+	echo -n "$article" 	# print article link
+	
+	# check if arctile text contains at least one name
+	if [[ $(grep -oP "Netanyahu|Gvir|Lapid|Gantz" <<< "$data" | wc -l) -gt 0 ]]; then
+		
+		# for each politician name in a article 
+		for name in Netanyahu Gvir Lapid Gantz; do
+
+			# name occurences count 
+			count=$(grep -o $name <<< "$data" | wc -l)
+		
+			# print name with occurence count or - if count is zero
+			echo -n ", $name, $( [[ $count -eq 0 ]] && echo "-" || echo "$count")"
+		done
+		
+		echo " "
+		
+	else
+		
+		echo ", - "	# print - if there are no names in this article
+		
+	fi
+		
+done
+
+
+
