583752af73b0d0df1d6b112a2b3e178d00950376
Roman Knyazev <antorichn@gmail.com>
2026-05-22 17:58:53 +0300

    final without grep fix

--- my_copy.c
+++ my_copy.c
@@ -20,19 +20,24 @@ void error();
 int main(int argc, char **argv) {
 	FILE *input, *output;
 
-	if (argc == 1) {
+	if (argc == 1) {	// use stdin/out if not provided another
 		input = stdin;
 		output = stdout;
 	} else {
+		// check if the first argument is "-" for stdin,
+		// otherwise open file for reading
 		if(!strcmp("-", argv[1])) input = stdin;
 		else input = fopen(argv[1], "r");
 		if(!input) error();		// open input error
 
+		// check if the second argument is missing or "-" for stdout,
+		// otherwise open file for writing
 		if(argc < 3 || !strcmp("-", argv[2])) output = stdout;
 		else output = fopen(argv[2], "w");
 		if(!output) error();	// open output error
 	}
 
+	// perform the file copy operation
 	copy(input, output);
 
 	// close opened files
@@ -43,6 +48,7 @@ int main(int argc, char **argv) {
 }
 
 void error() {
+	// print error message to stderr and terminate the program
 	fprintf(stderr, "Error occurred\n");
 	exit(1);
 }
@@ -50,6 +56,7 @@ void error() {
 void copy(FILE *input, FILE *output) {
 	int current_char;
 
+	// read and write character by character
 	while(1) {
 		current_char = fgetc(input);
 		if(current_char == EOF) {
@@ -59,5 +66,6 @@ void copy(FILE *input, FILE *output) {
 		if( fputc(current_char, output) == EOF) error();		// write error
 	}	
 
+	// ferror() checks if there is an error flag inside input (read error)
 	if(ferror(input)) error();		// read error
 }
--- my_grep.c
+++ my_grep.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*returns 1 if line matches somewhere in line the regex
-0 otherwise, uses matchhere*/
+/*
+	returns 1 if line matches somewhere in line the regex
+  	0 otherwise, uses matchhere()
+*/
 int match(char *text, char *regexp);
 
-/*returns 1 if line matches  the regex at the start of the line
-0 otherwise, uses matchstar*/
+/*
+  	returns 1 if line matches the regex at the start of the line
+	0 otherwise, uses matchstar()
+*/
 int matchhere(char *text, char *regexp);
 
-/*returns 1 if start of the line is c*regex
-0 otherwise, */
+/*
+ 	returns 1 if start of the line is c*regex
+	0 otherwise, uses matchhere()
+*/
 int matchstar(char *text, char c, char *regexp);
 
 #define BUFSIZE 256
@@ -112,30 +118,35 @@ int match(char *text, char *regexp) {
 
 int matchhere(char *text, char *regexp) {
 
+	// empty regex means successful match
 	if(regexp[0] == '\0') return 1;
 
+	// '$' matches the end of the text
 	if(regexp[0] == '$' && regexp[1] == '\0') {
 		return (text[0] == '\0');
 	}
 
+	// '*' means zero or more occurrences of the previous char
 	if(regexp[1] == '*') {
 		return matchstar(text, regexp[0], regexp + 2);
 	}
 
+	// match current char (exact or '.') and move to the next one
 	if(text[0] != '\0' && (regexp[0] == '.' || regexp[0] == text[0]) ) {
 		return matchhere(text + 1, regexp + 1);
 	}
 
-	return 0;
+	return 0; // no match found
 }
 
 int matchstar(char *text, char c, char* regexp) {
 
 	do {
-
+		// check if the rest of the text matches the rest of the regex
 		if(matchhere(text, regexp)) return 1;
 
+	// advance text as long as the current char matches 'c' (or 'c' is '.')
 	} while(*text != '\0' && (*text++ == c || c == '.'));
 
-	return 0;
+	return 0; // no match found
 }
