c2f1190df9ae88edca961665655a3c3c29dfe3af
Roman Knyazev <antorichn@gmail.com>
2026-06-03 18:56:24 +0300

    removed stack.h dependency from student.h

--- student.h
+++ student.h
@@ -1,16 +1,14 @@
 #ifndef HW3_STUDENT_H
 #define HW3_STUDENT_H
 
-#include "stack.h"
-
 struct student {
     char *name;
     int age;
     int id;
 };
 
-elem_t student_clone(elem_t e);
-void student_destroy(elem_t e);
-void student_print(elem_t e);
+void* student_clone(void* e);
+void student_destroy(void*e);
+void student_print(void* e);
 
 #endif //HW3_STUDENT_H
