c110f4e1cf846136e28623a869da35867d1dcb1d
Roman Knyazev <antorichn@gmail.com>
2026-06-07 14:59:42 +0300

    changed return values in stack.c/ stack.h; added comments in student.c

--- stack.c
+++ stack.c
@@ -3,7 +3,6 @@
 
 #include <stdlib.h>
 
-    // FIX FAILURE RETURN VALUES
 
 struct stack {
     size_t size;    // max elements count
@@ -17,16 +16,20 @@ struct stack {
     print_t print_func;
 };
 
+/*
+Input: size, user proved functions. Output: new stack instance
+Creates new stack instance using provided arguments
+*/
 struct stack* stack_create (
         size_t size,
         clone_t clone_func,
         destroy_t destroy_func,
         print_t print_func) {
 
-    struct stack* new_stack = malloc(sizeof(struct stack));
+    struct stack* new_stack = malloc(sizeof(struct stack));		// malloc for a new stack instance
     if (!new_stack) return NULL;    // memory allocation failure
 
-    elem_t* elements = malloc(size * sizeof(elem_t));
+    elem_t* elements = malloc(size * sizeof(elem_t));	// malloc for a stack elements array
     if (!elements) {
         free(new_stack);    // free memory allocated for new_stack in case of failure
         return NULL;     // memory allocation failure
@@ -43,65 +46,77 @@ struct stack* stack_create (
     return new_stack;
 }
 
-void stack_destroy(struct stack* stack) {
+// frees allocated memory of provided stack
+int stack_destroy(struct stack* stack) {
     if (stack) {
-        for (int i = 0; i <= stack->size; i++) {    // destroy each element with user provided function
+        for (int i = 0; i <= stack->size; i++) {    // destroy each array element using user provided function
             stack->destroy_func(*(stack->elements + i));
         }
+        
         free(stack->elements);
         free(stack);
 
-        return ;   // success
+        return SUCCESS;   // success
     }
+    
+    return ERROR;	// error
 }
 
+// adds provided element to provided stack
 int stack_push(struct stack* stack, elem_t e) {
     if (stack && (stack->elem_count < stack->size) ) {
-        elem_t e_clone = stack->clone_func(e);
-        if (!e_clone) return -1;    // clone failure
+    
+        elem_t e_clone = stack->clone_func(e);	// create copy of provided element
+        if (!e_clone) return ERROR;    // clone failure
 
-        *(stack->elements + stack->elem_count++) = e_clone;
-
-        return 0;
+        *(stack->elements + stack->elem_count++) = e_clone;		// put new element copy into array and increment elements count
+        
+        return SUCCESS;		// success
     }
-
-    return -1;
+    
+    return ERROR;	// error
 }
 
+// removes last entered element from provided stack
 void stack_pop(struct stack* stack) {
     if (stack && stack->elem_count > 0) {
-        stack->elem_count--;
-        stack->destroy_func(*(stack->elements + stack->elem_count));
+        stack->elem_count--;	// decrement elements count
+        stack->destroy_func(*(stack->elements + stack->elem_count));	// destroy last entered element with user provided function
     }
 }
 
+// returns pointer to last entered element of provided stack
 elem_t stack_peek(struct stack* stack) {
     if (stack && stack->elem_count > 0) {
-        return *(stack->elements + stack->elem_count - 1);
+        return *(stack->elements + stack->elem_count - 1);	
     }
 
     return NULL;
 }
 
+// returns provided stack elements count
 size_t stack_size(struct stack* stack) {
     if (stack) return stack->elem_count;
-    return 0;
+    return 0;	// error (returning 0 according to task requirement)
 }
 
+// returns if provided stack has no elements
 bool stack_is_empty(struct stack* stack) {
     if (stack) return stack->elem_count == 0;
-    return false;
+    return false;	// error
 }
 
+// return if there is space left for new elements in a provided stack
 size_t stack_capacity(struct stack* stack) {
     if (stack) return stack->size - stack->elem_count;
-    return 0;
+    return 0;   // error (returning 0 according to task requirement)
 }
 
+// prints provided stack elements starting from last entered element
 void stack_print(struct stack* stack) {
     if (stack && stack->elem_count > 0) {
-        for (int i = stack->elem_count - 1; i >= 0; i--) {
-            stack->print_func(*(stack->elements + i));
+        for (int i = stack->elem_count - 1; i >= 0; i--) {	// run a loop with decrementing counter 
+            stack->print_func(*(stack->elements + i));		// print array elements in a descending order
         }
     }
 }
--- stack.h
+++ stack.h
@@ -1,33 +1,52 @@
-//
-// Created by antor on 03.06.2026.
-//
-
 #ifndef HW3_STACK_H
 #define HW3_STACK_H
 #include <stdbool.h>
 #include <stddef.h>
 
+#define SUCCESS 0
+#define ERROR 1
+
 struct stack;
 
 typedef void *elem_t;
 
+// user provided functions
 typedef elem_t (*clone_t) (elem_t e);
 typedef void (*destroy_t) (elem_t e);
 typedef void (*print_t) (elem_t e);
 
+/*
+Input: size, user proved functions. Output: new stack instance
+Creates new stack instance using provided arguments
+*/
 struct stack* stack_create (
         size_t size,
         clone_t clone_func,
         destroy_t destroy_func,
         print_t print_func);
 
-void stack_destroy(struct stack* stack);
+// frees allocated memory of provided stack
+int stack_destroy(struct stack* stack);
+
+// adds provided element to provided stack
 int stack_push(struct stack* stack, elem_t e);
+
+// removes last entered element from provided stack
 void stack_pop(struct stack* stack);
+
+// returns pointer to last entered element of provided stack
 elem_t stack_peek(struct stack* stack);
+
+// returns provided stack elements count
 size_t stack_size(struct stack* stack);
+
+// returns if provided stack has no elements
 bool stack_is_empty(struct stack* stack);
+
+// return if there is space left for new elements in a provided stack
 size_t stack_capacity(struct stack* stack);
+
+// prints provided stack elements starting from last entered element
 void stack_print(struct stack* stack);
 
 #endif //HW3_STACK_H
